c87aba9f59a71b152217354303ac1016f8f204df
Surghe <Milesiii.Irving@gmail.com>
2025-05-06 14:08:56 -0600

    server.mjs boilerplate

---
+++ middleware/globalError.mjs
@@ -0,0 +1,4 @@
+export default function globalError (err, _req, res, next)  {
+    res.status(500).json({msg: err.message})
+};
+
---
+++ server.mjs
@@ -0,0 +1,32 @@
+// Imports 
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors"
+import globalError from "./middleware/globalError.mjs";
+
+
+// Setups 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+// Midware
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+
+
+
+// Routes
+
+// Err Midware
+app.use(globalError);
+
+
+// Listener
+app.listen(PORT, ()=>{
+    console.log(`Server running on Port: ${PORT}`)
+});
